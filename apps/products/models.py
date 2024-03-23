b0382e313dc9f7924ce220f896336cdd7d95ae8a
Chyngyz <konuratov.chika@gmail.com>
2024-03-23 16:52:23 +0600

    Initial commit

---
+++ apps/products/models.py
@@ -0,0 +1,23 @@
+# from django.db import models
+
+# # Create your models here.
+
+# # class Bucket(models.Model):
+
+# class Category(models.Model):
+#     title = models.CharField(max_length=150, verbose_name="Название")
+
+#     class Meta:
+#         verbose_name = 'Категория'
+#         verbose_name_plural = 'Категории'
+
+# class Product(models.Model):
+#     title = models.CharField(max_length=150, verbose_name="Название")
+#     photo = models.ImageField(upload_to='products', verbose_name='Картинка')
+#     description = models.TextField(verbose_name='Описание')
+#     detailed_description = models.TextField(verbose_name='Полное описание')
+#     price = models.DecimalField(verbose_name='Цена')
+
+#     class Meta:
+#         verbose_name = ''
+#         verbose_name_plural = ''
